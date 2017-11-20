0a201d3ce052c7bc616fe554da0fbdcaee730ea3
yiwei <william.tseng@sendbest.cc>
2017-11-20 09:26:33 +0800

    server code

---
+++ src/client/routes/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <div>I'm the home page</div>
+    <button>Press me!</button>
+  </div>
+);
+
+export default Home;
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import Home from './client/routes/Home';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  const content = renderToString(<Home />);
+
+  res.send(content);
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
