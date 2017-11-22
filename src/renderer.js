a232dc8bc6c2205c0f49588269387eee27f5e0a2
yiwei <william.tseng@sendbest.cc>
2017-11-22 09:33:37 +0800

    mitigating xss sttacks

--- src/renderer.js
+++ src/renderer.js
@@ -3,6 +3,7 @@ import { renderToString } from 'react-dom/server';
 import { StaticRouter, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { renderRoutes } from 'react-router-config';
+import serialize from 'serialize-javascript';
 import routes from './client/routes';
 
 
@@ -23,7 +24,7 @@ export default (req, store) => {
       <body>
         <div id="root">${content}</div>
         <script>
-          window.INITIAL_STATE = ${JSON.stringify(store.getState())}
+          window.INITIAL_STATE = ${serialize(store.getState())}
         </script>
         <script src="bundle.js"></script>
       </body>
