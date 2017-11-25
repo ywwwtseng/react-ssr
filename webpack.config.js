ae4de934a3839e3cf1a9b30446f5c87fb0bdccdf
yiwei <william.tseng@sendbest.cc>
2017-11-25 14:39:27 +0800

    refactoring webpack config

---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+const path = require('path');
+const WebpackNodeExternals = require('webpack-node-externals');
+
+//
+// Configuration for the client-side bundle (client)
+// -----------------------------------------------------------------------------
+const clientConfig = {
+  // Tell webpack the root file of our
+  // server application
+  entry: [
+    'babel-polyfill',
+    './src/client/index.js'
+  ],
+
+  // Tell webpack where to put the output file
+  // that is generated
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'client')
+  },
+
+  // Tell webpack to run babel on every file it runs through
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules/',
+        options: {
+          presets: [
+            'react',
+            'stage-0',
+            ['env', { targets: { browsers: ['last 2 versions'] } }]
+          ],
+          compact: true
+        }
+      }
+    ]
+  }
+};
+
+//
+// Configuration for the server-side bundle (server)
+// -----------------------------------------------------------------------------
+const serverConfig = {
+  // Inform webpack that we're building a bundle
+  // for nodeJS, rather than for the browser.
+  target: 'node',
+
+  // Tell webpack the root file of our
+  // server application
+  entry: './src/index.js',
+
+  // Tell webpack where to put the output file
+  // that is generated
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'server')
+  },
+
+  // Tell webpack to run babel on every file it runs through
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules/',
+        options: {
+          presets: [
+            'react',
+            'stage-0',
+            ['env', { targets: { node: 'current' } }]
+          ],
+          compact: true
+        }
+      }
+    ]
+  },
+
+  externals: [WebpackNodeExternals()]
+};
+
+module.exports = [clientConfig, serverConfig];
