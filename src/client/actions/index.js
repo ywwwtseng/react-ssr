2a1939041c0a398068777a9cca0bdebb7d0110b4
YW-Tseng <sers810112@hotmail.com>
2017-12-08 16:27:01 +0800

    clean action.js

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -2,16 +2,13 @@ import history from '../history';
 
 export const USER_LOGIN = 'USER_LOGIN';
 export const userLogin = ({ username, password }) => async (dispatch, getState, api) => {
-
   try {
     const res = await api.post('/login', { username, password });
 
-    dispatch({
-      type: USER_LOGIN,
-      payload: res
-    });
+    dispatch({ type: USER_LOGIN, payload: res });
 
     history.push('/');
+
   } catch (error) {
     throw error;
   }
@@ -19,20 +16,15 @@ export const userLogin = ({ username, password }) => async (dispatch, getState, 
 
 export const USER_LOGOUT = 'USER_LOGOUT';
 export const userLogout = () => async (dispatch, getState, api) => {
-  const { auth } = getState();
+  if (!getState().auth) return;
 
-  if (!auth) return;
-
-  dispatch({
-    type: GET_CURRENT_USER,
-    payload: false
-  });
+  dispatch({ type: GET_CURRENT_USER, payload: false });
 
   try {
     await api.post('/logout');
 
   } catch (error) {
-    console.error(error);
+    throw error;
   }
 };
 
@@ -41,78 +33,48 @@ export const getCurrentUser = () => async (dispatch, getState, api) => {
   try {
     const res = await api.get('/current_user');
 
-    dispatch({
-      type: GET_CURRENT_USER,
-      payload: res
-    });
+    dispatch({ type: GET_CURRENT_USER, payload: res });
+
   } catch (error) {
-    dispatch({
-      type: GET_CURRENT_USER,
-      payload: false
-    });
+    dispatch({ type: GET_CURRENT_USER, payload: false });
   }
 };
 
 export const GET_USERS = 'GET_USERS';
 export const getUsers = socket => async (dispatch, getState, api) => {
   try {
-
     if (process.env.BROWSER && socket) {
 
       socket.emit('user-list');
-      socket.on('user-list-response', (data) => {
 
-        dispatch({
-          type: GET_USERS,
-          payload: data
-        });
-      });
+      socket.on('user-list-response', data => dispatch({ type: GET_USERS, payload: data }));
 
     } else {
-
       const res = await api.get('/users');
 
-      dispatch({
-        type: GET_USERS,
-        payload: res
-      });
+      dispatch({ type: GET_USERS, payload: res });
 
     }
   } catch (error) {
-
     throw error;
-
   }
 };
 
 export const GET_MESSAGES = 'GET_MESSAGES';
 export const getMessages = socket => async (dispatch, getState, api) => {
   try {
-
     if (process.env.BROWSER && socket) {
-
       socket.emit('messages');
-      socket.on('messages-response', (data) => {
 
-        dispatch({
-          type: GET_MESSAGES,
-          payload: data
-        });
-      });
+      socket.on('messages-response', data => dispatch({ type: GET_MESSAGES, payload: data }));
 
     } else {
-
       const res = await api.get('/messages');
 
-      dispatch({
-        type: GET_MESSAGES,
-        payload: res
-      });
+      dispatch({ type: GET_MESSAGES, payload: res });
 
     }
   } catch (error) {
-
     throw error;
-
   }
 };
