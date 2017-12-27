a90216e4633350ce3ff7b7e8a2966d084b3e4b09
YW-Tseng <sers810112@hotmail.com>
2017-12-27 15:45:21 +0800

    webpack config

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -1,24 +1,25 @@
 const path = require('path');
 const webpack = require('webpack');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const WebpackNodeExternals = require('webpack-node-externals');
 const WebpackShellPlugin = require('webpack-shell-plugin');
 
 //
-// Configuration for the client-side bundle (client)
+// Configuration for the server-side bundle (server)
 // -----------------------------------------------------------------------------
-const clientConfig = {
+const serverConfig = {
+  // Inform webpack that we're building a bundle
+  // for nodeJS, rather than for the browser.
+  target: 'node',
+
   // Tell webpack the root file of our
   // server application
-  entry: [
-    'babel-polyfill',
-    './src/client/index.js'
-  ],
+  entry: './src/index.js',
 
   // Tell webpack where to put the output file
   // that is generated
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'client')
+    path: path.resolve(__dirname, 'server')
   },
 
   // Tell webpack to run babel on every file it runs through
@@ -32,30 +33,24 @@ const clientConfig = {
           presets: [
             'react',
             'stage-0',
-            ['env', { targets: { browsers: ['last 2 versions'] } }]
+            ['env', { targets: { node: 'current' } }]
           ],
           compact: true,
           plugins: ['transform-class-properties', 'babel-plugin-styled-components']
+
         }
-      },
+      }
     ]
   },
 
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.BROWSER': true
+      'process.env.BROWSER': false
     }),
+    new WebpackShellPlugin({ onBuildEnd: [ "nodemon --watch server --exec node server/bundle.js" ] })
+  ],
 
-    // new CopyWebpackPlugin([
-    //   {
-    //     from: path.resolve(__dirname, 'src/client/assets'),
-    //     to: path.resolve(__dirname, 'client/assets')
-    //   }
-    // ]),
-
-    new WebpackShellPlugin({ onBuildEnd: [ "webpack --config webpack.server.config.js --watch" ] })
-  ]
+  externals: [WebpackNodeExternals()]
 };
 
-
-module.exports = clientConfig;
+module.exports = serverConfig;
