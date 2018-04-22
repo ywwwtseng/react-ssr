02e6663d8156dea4d61532d6f88cf63d7396a5ed
yiwei <yiwei@yiweide-MacBook-Pro.local>
2018-04-22 14:36:41 +0800

    test

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import apiSocketServer from './api-socket-server';
 import webServer from './web-server/index';
 import config from './config';
 
+// test
+
 apiSocketServer.listen(config.SOCKET_SEVER_PORT);
 webServer.listen(config.WEB_SEVER_PORT);
